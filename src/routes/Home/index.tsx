c545bc5c50de0cffb722d1c1fa2793d8dd48efa8
Ital023 <contadoitalo911@gmail.com>
2024-08-12 19:42:48 -0300

    fix: fixing bugs

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -6,12 +6,23 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="dsct-container">
-        <section>
-          <h2 className="dsct-section-title">Venha nos visitar</h2>
-          <div className="dsct-car-cards">
-            <CarCard/>
-            <CarCard/>
+      <main>
+        <section className="dsct-section-products">
+          <div className="dsct-section-content dsct-container">
+            <h2 className="dsct-section-title">Venha nos visitar</h2>
+            <div className="dsct-car-cards">
+            <CarCard />
+            <CarCard />
+            </div>
+
+            
+          </div>
+        </section>
+
+        <section className="dsct-section-faq">
+          <div className="dsct-section-content dsct-container">
+            <h2 className="dsct-section-title">O que estão dizendo</h2>
+            <div className="dsct-faq"></div>
           </div>
         </section>
       </main>
