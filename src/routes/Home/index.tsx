0ec28c7ad17c31a537582ddd4c004d0d664c1207
Ital023 <contadoitalo911@gmail.com>
2024-08-12 20:28:40 -0300

    feat: Comments card finished

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,4 +1,5 @@
 import CarCard from "../../components/CarCard";
+import CommentsCard from "../../components/CommentsCard";
 import Header from "../../components/Header";
 import "./styles.css";
 
@@ -6,9 +7,10 @@ export default function Home() {
   return (
     <>
       <Header />
+      
       <main>
         <section className="dsct-section-products">
-          <div className="dsct-section-content dsct-container">
+          <div className="dsct-container">
             <h2 className="dsct-section-title">Venha nos visitar</h2>
             <div className="dsct-car-cards">
             <CarCard />
@@ -19,13 +21,22 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="dsct-section-faq">
-          <div className="dsct-section-content dsct-container">
+        <section className="dsct-section-comments">
+          <div className="dsct-container-comments">
             <h2 className="dsct-section-title">O que estão dizendo</h2>
-            <div className="dsct-faq"></div>
+            <div className="dsct-comments">
+              <CommentsCard/>
+              <CommentsCard/>
+              <CommentsCard/>
+              <CommentsCard/>
+              <CommentsCard/>
+
+            </div>
           </div>
         </section>
       </main>
+
+
     </>
   );
 }
